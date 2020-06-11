5dbddc951b5c9458ed1e2438af32982ac0c8d19c
clemence-desrousseaux <66476817+clemence-desrousseaux@users.noreply.github.com>
2020-06-11 12:04:50 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -49,3 +49,55 @@ def send_json_country(self,country) :
   
   #On renvoie maintenant un dictionnaire au format JSON
   else : 
+    #On note k la clé primaire de notre base de données
+    data = {k:p[k] for k in p.()}
+    headers = [('Content-Type','application/json')]
+    self.send_json(data,headers)
+  
+  
+  
+ #On récupère maintenant les données des pays 
+
+def data_loc(self) : 
+  
+  #On prépare la requête SQL
+  comm = conn.cursor()
+  sql = 'SELECT * from countries'
+  
+  #On récupère l'information
+  comm.execute(sql)
+  
+  #On sélectionne les données présentes dans la base de données
+  p = comm.fetchall()
+  data = []
+  for i in p : 
+    wp = i['wp']
+    lat = i['latitude']
+    long = i['longitude']
+    name = i['name']
+    data.append({'wp' : wp, 'lat' : lat, 'lon' : lon, 'name' : name})
+  return data
+
+
+
+#On continue de récupérer les données d'un pays 
+
+def db_get_country(self,country) : 
+  comm = conn.cursor()
+  sql = 'SELECT * from countries WHERE wp = ?'
+  comm.execute(sql,(country,))
+  return comm.fetchone()
+
+
+#Finalement, on ouvre une connexion avec la base de données 
+
+connection = sqlite3.connect('pays.sqlite')
+connection.row_factory = sqlite3.Row     #On accède au résultat des enquêtes sous forme d'un dictionnaire
+
+#Enfin, on instancie et on lance le serveur
+
+httpd = socketserver.TCPServer(("",8081),RequestHandler)
+httpd.serve_forever()
+
+
+
