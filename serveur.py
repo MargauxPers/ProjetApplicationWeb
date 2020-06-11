7f38204b98e8d02bff7971076d0661b426562e89
clemence-desrousseaux <66476817+clemence-desrousseaux@users.noreply.github.com>
2020-06-11 11:47:13 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -39,39 +39,13 @@ On surcharge la méthode qui traite les requêtes GET
 #On renvoie désormais les informations de chaque pays au format json
 
 def send_json_country(self,country) :
-  # on récupère le pays depuis la base de données
-    r = self.db_get_country(country)
-
-    # on n'a pas trouvé le pays demandé
-    if r == None:
-      self.send_error(404,'Country not found')
-
-    # on renvoie un dictionnaire au format JSON
-    else :
-      #k correspond à la clef primaire de notre base de donnée
-      data = {k:r[k] for k in r.keys()}
-      headers = [('Content-Type','application/json')]
-      self.send_json(data,headers)
   
+  #On récupère tout d'abord le pays à partir de la base de données
+  p = self.db_get_country(country)
   
+  #Si on ne trouve pas le pays demandé 
+  if p == None
+    self.send_error(404,'Pays introuvable')
   
-def data_loc(self) :
-    #Préparation de la requête SQL
-    c = conn.cursor()
-    sql = 'SELECT * from countries'
-
-    #Récupération de l'information
-    c.execute(sql)
-    #Sélection des données présentes dans la base de donnée
-    r = c.fetchall()
-    data = []
-    for i in r :
-      wp = i['wp']
-      lat = i['latitude']
-      lon = i['longitude']
-      name = i['name']
-      data.append({'wp': wp, 'lat': lat, 'lon': lon, 'name': name})
-    return data
-  
-  
-  
+  #On renvoie maintenant un dictionnaire au format JSON
+  else : 
